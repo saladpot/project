011d00ba996cc76a9ab146a13130d436ffcfae11
potsalad <117halorocks@gmail.com>
2015-07-29 10:58:26 -0400

    Summary
    Descripition

---
+++ omo.js
@@ -0,0 +1,41 @@
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function(obj) {
+    var slice = [].slice,
+        args  = slice.call(arguments, 1),
+        self  = this,
+        nop   = function () {},
+        bound = function () {
+          return self.apply(this instanceof nop ? this : (obj || {}), args.concat(slice.call(arguments)));   
+        };
+    nop.prototype   = self.prototype;
+    bound.prototype = new nop();
+    return bound;
+  };
+}
+
+if (!Object.create) {
+  Object.create = function(base) {
+    function F() {};
+    F.prototype = base;
+    return new F();
+  }
+}
+
+if (!Object.construct) {
+  Object.construct = function(base) {
+    var instance = Object.create(base);
+    if (instance.initialize)
+      instance.initialize.apply(instance, [].slice.call(arguments, 1));
+    return instance;
+  }
+}
+
+if (!Object.extend) {
+  Object.extend = function(destination, source) {
+    for (var property in source) {
+      if (source.hasOwnProperty(property))
+        destination[property] = source[property];
+    }
+    return destination;
+  };
+}
